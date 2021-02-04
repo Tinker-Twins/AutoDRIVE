3a7dbefbe185de80ca9c7ce20e711c4ba37171c5
Tinker Twins <tinkertwins22@gmail.com>
2021-02-04 23:04:11 +0530

    Initial commit

--- autodrive_cpp/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script to execute the compiled project files.
-
-# Go into the directory where the compiled files of the project are contained.
-cd build
-
-# Run the compiled executable
-./autodrive
--- autodrive_py/README.md
+++
@@ -1,3 +0,0 @@
-# AutoDRIVE SDK | Python
-
-This directory hosts the Python script, which can be used to easily develop autonomous driving algorithms, especially those employing learning-based techniques.
