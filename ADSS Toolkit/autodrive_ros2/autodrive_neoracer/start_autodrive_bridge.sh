17a21d3419976ca373105536f51ad3b233ad20b9
Tinker Twins <tinkertwinsofficial@gmail.com>
2026-09-21 04:23:54 -0400

    Delete start_autodrive_bridge.sh

--- ADSS Toolkit/autodrive_ros2/autodrive_neoracer/start_autodrive_bridge.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Start the NeoRacer AutoDRIVE stack: Socket.IO bridge (:4567) plus the
-# real driver's mux and throttle nodes, so /drive flows through the same
-# chain as on hardware.
-source /opt/ros/humble/setup.bash
-source "$HOME/ros2_ws/install/setup.bash"
-export FASTRTPS_DEFAULT_PROFILES_FILE="$HOME/fastdds_udp.xml"
-exec ros2 launch autodrive_neoracer autodrive.launch.py
